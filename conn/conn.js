75efd9d59f125b5730ded257d51ac8e619a917a6
SASHIKANT KUMAR <s.k.kumar23pu@gmail.com>
2026-08-28 23:51:22 +0530

    Update conn.js

--- conn/conn.js
+++ conn/conn.js
@@ -3,7 +3,6 @@ const mongoose = require("mongoose");
 const conn = async () => {
   try {
     console.log("Connecting to MongoDB...");
-    console.log(process.env.MONGO_URI);
 
     await mongoose.connect(process.env.MONGO_URI);
 
